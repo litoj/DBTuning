9f85f99dbc4f2f91e06fa6daa6b23075e424f82a
litoj <invisiblemancz@gmail.com>
2025-03-18 22:59:43 +0100

    try turning off null check for import

--- HW1/automated_approaches.sh
+++ HW1/automated_approaches.sh
@@ -1,40 +1,58 @@
 #!/bin/bash
 
 cleanup() {
-	dropdb dblp
-	createdb dblp
+    dropdb dblp
+    createdb dblp
 }
 
+
 format_md_result() {
-	echo '```'
-	time "$@"
-	echo '```'
+	echo time "$@"
 }
 
 run_test() {
-	echo "## Approach _${1//_/ → }_"
+	echo "Approach ${1//_/ → }"
 
 	cleanup
 
 	if [[ $DETAILED ]]; then
-		echo "### Individual"
+		echo "Individual:"
 		while read -r cmd; do # time each command separately
 			echo "- $cmd"
 			eval "args=(${cmd%;})"
-			format_md_result "${args[@]}"
+			time "${args[@]}"
 		done < <(declare -f "$1" | tail -n +3 | head -n -1) # extract the commands inside the function
 
 		cleanup
 	fi
 
-	echo "### Summary"
-	format_md_result "$1" # time the entire procedure as a single unit of work
+	echo "Summary:"
+	time "$1" # time the entire procedure as a single unit of work
 }
 
-structure=$(sed '/ALTER/d' create.sql)
-constraints=$(grep 'ALTER' create.sql)
-sql_copy_publ="\copy publ from 'publ.tsv' delimiter E'\\t'"
-sql_copy_auth="\copy auth from 'auth.tsv' delimiter E'\\t'"
+sql_copy_publ="\copy publ from 'dblp/publ.tsv' delimiter E'\\t'"
+sql_copy_auth="\copy auth from 'dblp/auth.tsv' delimiter E'\\t'"
+
+# Approach 1: Constraints First
+constraints_first() {
+	psql -d dblp -f 'create2.sql'
+	psql -d dblp -c "$sql_copy_publ"
+	psql -d dblp -c "$sql_copy_auth"
+}
+
+# Approach 2: Constraints Last (Structure → Data → Constraints)
+structure_data_constraints() {
+	structure=$(sed '/ALTER/d' create1.sql)
+	constraints=$(grep 'ALTER' create1.sql)
+
+	psql -d dblp -c "$structure"
+	psql -d dblp -c "$sql_copy_publ"
+	psql -d dblp -c "$sql_copy_auth"
+	psql -d dblp -c "$constraints"
+}
+
+
+# Additional approaches we didn't try:
 
 create_populate() {
 	psql -d dblp -f 'create.sql'
@@ -42,13 +60,16 @@ create_populate() {
 	psql -d dblp -c "$sql_copy_auth"
 }
 
-structure_data_constraints() {
-	psql -d dblp -c "$structure"
+minimal_preparation() {
+	psql -d dblp -c "${structure//NOT NULL/}"
 	psql -d dblp -c "$sql_copy_publ"
 	psql -d dblp -c "$sql_copy_auth"
+	psql -d dblp -c "$(echo "$structure" | sed -n 's/CREATE \(TABLE .*\) (/ALTER \1/p;s/\s*\(\S\+\) .* \(NOT NULL\)/ALTER COLUMN \1 SET \2/p;s/);/;/p' )"
 	psql -d dblp -c "$constraints"
 }
 
-run_test create_populate
 
+# Run both approaches for comparison
+run_test constraints_first
 run_test structure_data_constraints
+
